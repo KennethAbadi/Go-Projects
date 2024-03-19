a86c7290bdd24356c0f021e12231fad44393a3b4
Kenneth Abadi <kenneth.putra25@gmail.com>
2024-03-19 15:50:37 -0700

    Deck

--- Deck of cards/Deckofcards.go
+++ Deck of cards/Deckofcards.go
@@ -1,1 +1,40 @@
-package main
+//go:generate stringer -type=Suit,Value
+package deck
+
+type Suit uint8 //Spade Diamond Club Heart, Joker for extra
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+type Value uint8 //2,3,4,5,6,7,8,9,10,j,q,k,a
+
+const (
+	x Value = iota //blank value
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+type Card struct {
+	Suit
+	Value
+}
+
+func (c Card) String() string {
+
+}
--- Quiz-game/go.mod
+++ Quiz-game/go.mod
@@ -1,3 +1,8 @@
 module github.com/KennethAbadi/Go-Projects/tree/main/Quiz-game
 
 go 1.22.1
+
+require (
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
+)
