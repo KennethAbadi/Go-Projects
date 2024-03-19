4f05621f1b59c0ff84a280049643d4aa414e37b6
Kenneth Abadi <kenneth.putra25@gmail.com>
2024-03-19 15:09:34 -0700

    Completed the Quiz Game
    Quiz game works

---
+++ Quiz-game/go.mod
@@ -0,0 +1,3 @@
+module github.com/KennethAbadi/Go-Projects/tree/main/Quiz-game
+
+go 1.22.1
--- Quiz-game/quiz.go
+++ Quiz-game/quiz.go
@@ -1,9 +1,98 @@
-package quiz
+package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
 	"log"
+	"os"
+)
+
+type question struct {
+	question string
+	answer   string
+}
+
+type quiz struct {
+	answered  int
+	correct   int
+	questions []question
+}
+
+func errormsg(message string, err error) {
+	if err != nil {
+		log.Fatalln(message, ":", err)
+	}
+	//Provides error when file isnt read.
+
+}
+
+func readfile(filepath string) *quiz {
+	csvFile, err := os.Open(filepath)
+	errormsg("Error opening CSV file", err)
+	defer csvFile.Close()
+
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	var quiz quiz
+
+	for {
+		line, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		errormsg("Error", err)
+		question := question{line[0], line[1]}
+
+		quiz.questions = append(quiz.questions, question)
+	}
+
+	return &quiz
+}
+
+func (quiz *quiz) run() {
+
+	for _, question := range quiz.questions {
+		fmt.Println(question.question)
+		answerchan := make(chan string)
+
+		go func() {
+			scanner.Scan()
+			answer := scanner.Text()
+			answerchan <- answer
+		}()
+
+		select {
+		case answer := <-answerchan:
+			if answer == question.answer {
+				quiz.correct++
+			}
+			quiz.answered++
+		}
+	}
+	return
+}
+
+func (quiz *quiz) output() {
+	fmt.Printf(
+		"You answered %v questions out of %v, you got %v questions correct",
+		quiz.answered,
+		len(quiz.questions),
+		quiz.correct,
+	)
+}
+
+var (
+	scanner     = bufio.NewScanner(os.Stdin)
+	filePathPtr = flag.String("file", "./problems.csv", "Path to csv file containing quiz.")
 )
 
 func main() {
-	log.Print(hello)
+	quiz := readfile(*filePathPtr)
+	quiz.run()
+	quiz.output()
 }
